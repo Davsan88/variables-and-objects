f174cb974d7145e18b6d1a42a370dc9b91ff8330
DavSan <david.sanchez.martinez@outlook.com>
2024-08-31 18:03:42 +0100

    Update VariableDisplay.jsx

--- src/VariableDisplay.jsx
+++ src/VariableDisplay.jsx
@@ -4,5 +4,8 @@ function VariableDisplay() {
     let strVar = 'Hello There';
     let numVar = 9;
     let booVar = true;
+    let arrVar = ['prequels', 'originals', 'sequels'];
+    let objVar = { name: 'Obiwan', age: '40', role: 'Jedi Master' };
+
     
 }
