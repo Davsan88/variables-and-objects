70a5a37fb51c136658ca78ac85f6c443f8560f88
DavSan <david.sanchez.martinez@outlook.com>
2024-08-31 19:20:12 +0100

    Update VariableDisplay.jsx

--- src/VariableDisplay.jsx
+++ src/VariableDisplay.jsx
@@ -8,6 +8,10 @@ function VariableDisplay() {
     let arrVar = ['prequels', 'originals', 'sequels'];
     let objVar = { name: 'Obiwan', age: '40', role: 'Jedi Master' };
 
+    // Conditional statement to chage the value of one of the variables
+    if (Math.random() >= 0.5) {
+        let strVar = 'Captain Kenobi!';
+    }
 
 }
 
